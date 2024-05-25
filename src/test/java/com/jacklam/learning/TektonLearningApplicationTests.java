a7eeac1e27ea2c94176c998c878826e91d11aa6e
jacklam1211 <kwanho.jack.lam@gmail.com>
2024-05-25 16:01:21 +0800

    spring-boot-template with a example endpoint

---
+++ src/test/java/com/jacklam/learning/TektonLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacklam.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TektonLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
